c9343180d7b2ce755648cc7662e472ffeac9cbef
Loïc LESCOAT <loic@example.com>
2024-09-01 23:51:08 -0700

    increment; order

--- app.py
+++ app.py
@@ -19,18 +19,26 @@ app = Flask(__name__)
 def home():
     conn = sqlite3.connect("dance-progress.db")
     cur = conn.cursor()
-    dances_list = cur.execute("select * from dance_progress order by name").fetchall()
+    dances_list = cur.execute(
+        "select * from dance_progress order by status desc, name asc"
+    ).fetchall()
     conn.close()
     dances = [dance(*x) for x in dances_list]
     return render_template("home.html", dances=dances)
 
 
-@app.route("/set_status/", methods=["GET"])
+@app.route("/increment/", methods=["GET"])
 def set_status():
     id = int(request.args["id"])
-    status = int(request.args["status"])
     conn = sqlite3.connect("dance-progress.db")
     cur = conn.cursor()
-    cur.execute("update dance_progress set status = ? where id = ?", (status, id))
+    status = int(
+        cur.execute("select status from dance_progress where id = ?", (id,)).fetchone()[
+            0
+        ]
+    )
+    cur.execute(
+        "update dance_progress set status = ? where id = ?", ((status + 1) % 3, id)
+    )
     conn.commit()
     return redirect(url_for("home"))
